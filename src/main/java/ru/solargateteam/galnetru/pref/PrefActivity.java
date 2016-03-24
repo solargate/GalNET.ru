a8ce01f590fcb95723e3c35f60b71df83dd165c6
Alexander Cheryomukhin <solarwind.palm@gmail.com>
2016-03-24 14:17:33 +0000

    [W][0.0.1] PrefActivity - наследник AppCompatActivity для использования штатного тулбара

--- src/main/java/ru/solargateteam/galnetru/pref/PrefActivity.java
+++ src/main/java/ru/solargateteam/galnetru/pref/PrefActivity.java
@@ -1,16 +1,17 @@
 package ru.solargateteam.galnetru.pref;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 
 import ru.solargateteam.galnetru.R;
 
-public class PrefActivity extends Activity {
+public class PrefActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefFragment()).commit();
     }
 
